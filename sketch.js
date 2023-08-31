42b73b00cc6d0b4f36620bdcdb1d308dd6c86bc1
khadijamishkat <143740368+khadijamishkat@users.noreply.github.com>
2023-08-31 20:56:13 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box 
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,20,40)
+}
+
+function draw() 
+{
+  background(30);
+  if(keyDown('up')){
+    box.y = box.y - 3
+  }
+  if(keyDown('down')){
+    box.y = box.y + 3
+    }
+  if(keyDown('right')){
+    box.x = box.x + 3
+  }
+  if(keyDown('left')){
+    box.x = box.x - 3
+  }
+  drawSprites()
+}
+
+
+
+
